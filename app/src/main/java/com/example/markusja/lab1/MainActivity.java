57ace34a0048cf85c94affce13b7018b1f569efe
MarkusJacobsen <markusja@stud.ntnu.no>
2018-01-25 09:31:48 +0100

    Proper exit in MainActivity

--- app/src/main/java/com/example/markusja/lab1/MainActivity.java
+++ app/src/main/java/com/example/markusja/lab1/MainActivity.java
@@ -51,7 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        moveTaskToBack(true);
         android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
     }
 
     public void addItemsSpinner(String selected) {
